99ac1e7f8b664f279bc5d19c674636100a9ecec3
2874160799 <anyuehuanye@qq.com>
2024-10-30 10:39:37 +0800

    v1.0 可正确悬停至1m bug:不确定原因是什么，有时候运行飞机会不动，需要ctrl+c停止后再次运行

--- hover.py
+++ hover.py
@@ -11,15 +11,18 @@ upward = 0.4
 angular = 0.0
 arm_flag = False
 start_flag = True
-position = None
 time_flag = 1
+hover_flag = False
+run_once_flag = True
+
+
 def pose_callback(msg):
   position = msg.pose.position
-  rospy.loginfo(f"Position - x:{position.x},y:{position.y},z:{position.z}")
-  
-def pose_listener():
-  rospy.Subscriber('/iris_0/mavros/local_position/pose',PoseStamped,pose_callback)
-  rospy.spin()
+  if position.z > 1.0:
+    global hover_flag 
+    hover_flag = True
+    #rospy.loginfo(f"Position - x:{position.x},y:{position.y},z:{position.z}")
+
 
 if __name__ == "__main__":
   rospy.init_node('iris_0_hover',anonymous=True)
@@ -28,9 +31,9 @@ if __name__ == "__main__":
   multi_cmd_vel_flu_pub = rospy.Publisher("/lyl/iris_0/cmd_vel_flu",Twist,queue_size=1)
   multi_cmd_pub = rospy.Publisher("/lyl/iris_0/cmd",String,queue_size=1)
   start_time = time.time()
-
-  while not rospy.is_shutdown():
-    #pose_listener()
+  rospy.Subscriber('/iris_0/mavros/local_position/pose',PoseStamped,pose_callback)
+  
+  while not rospy.is_shutdown():  
     while time_flag :
       for i in range(3,0,-1):
         print(f"The Drone will Arming after {i}s")
@@ -48,8 +51,19 @@ if __name__ == "__main__":
         arm_flag = False
         multi_cmd_pub.publish(cmd)
         print('arming')
+    if hover_flag == True and run_once_flag == True:
+      cmd = 'HOVER'
+      print('hover')
+      multi_cmd_pub.publish(cmd)
+      hover_flag = False
+      run_once_flag = False
+      forward = 0.0
+      leftward = 0.0
+      upward = 0.0
+      angular = 0.0
     twist.linear.x = forward; twist.linear.y = leftward ; twist.linear.z = upward
     twist.angular.x = 0.0; twist.angular.y = 0.0;  twist.angular.z = angular
     multi_cmd_vel_flu_pub.publish(twist)
     multi_cmd_pub.publish(cmd)
     cmd = ''
+    
