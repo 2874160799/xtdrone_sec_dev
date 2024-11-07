5fd635570b3822208ca133a725b9afa1d66855b9
2874160799 <anyuehuanye@qq.com>
2024-11-07 15:13:40 +0800

    正常规划路线+rviz发布目标

--- catkin_rl/src/move_base_publisher/src/main.cpp
+++ catkin_rl/src/move_base_publisher/src/main.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <thread>
 #include <send_goal.h>
+
 int main(int argc, char** argv) 
 {
     ros::init(argc, argv, "send_goal_node");
@@ -9,24 +11,22 @@ int main(int argc, char** argv)
     double x = nh.param("goal_x",1.0);
     double y = nh.param("goal_y",1.0);
 
-    ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker",10);
+    ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker",0);
     geometry_msgs::PoseStamped goal_pose;
     goal_pose.header.frame_id = "map";
     goal_pose.pose.position.x = x;    
     goal_pose.pose.position.y = y;   
     goal_pose.pose.orientation.w = 1.0;//朝向为默认
-    send_goal(x,y,1.0);
+    std::thread goal_thread(send_goal,x,y,1.0);
+
     ros::Rate loop_rate(10);
 
-    while(ros::ok)
+    while(ros::ok())
     {
         publish_goal_marker(marker_pub,goal_pose);
-        // send_goal(x,y,1.0);
         ros::spinOnce();
         loop_rate.sleep();
     }
-
-    
+    goal_thread.join();
     return 0;
-
 }
